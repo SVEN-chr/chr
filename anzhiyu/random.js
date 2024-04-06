0101e357d53b462c71dc3764c0e967e9cef035b3
SVEN-chr <chr1768902202@126.com>
2024-04-06 18:58:05 +0800

    Site updated: 2024-04-06 18:58:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/04/hello-world/"];function toRandomPost(){
+var posts=["2024/04/06/Microsoft  Activation  Scripts (MAS)/","2024/04/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
