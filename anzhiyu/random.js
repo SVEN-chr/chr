94f0ebbae1c45abc49aeffb76c4d53f26c557b12
SVEN-chr <chr1768902202@126.com>
2024-04-06 23:57:14 +0800

    Site updated: 2024-04-06 23:57:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/06/Microsoft  Activation  Scripts (MAS)/","2024/04/04/hello-world/"];function toRandomPost(){
+var posts=["2024/04/06/Microsoft  Activation  Scripts (MAS)/","2024/04/04/hello-world/","2024/04/06/kali的常用命令/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
